209f0eead5d1703866b691d0b24b71a016586b30
Daniel Holden <theonlydancingbanana@hotmail.com>
2014-01-06 12:30:54 -0500

    Added tracking

--- lispy.py
+++ lispy.py
@@ -40,6 +40,17 @@ header = """
       <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
       <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
     <![endif]-->
+  
+  <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-46885107-1', 'buildyourownlisp.com');
+    ga('send', 'pageview');
+  </script>
+  
   </head>
   <body style="background: url(static/img/tiletop.png) repeat-x;">
   
