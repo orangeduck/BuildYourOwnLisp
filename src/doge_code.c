9e844d6161ca6968b48b56a62d11bcbcf0c080dc
Daniel Holden <contact@theorangeduck.com>
2014-01-13 18:49:25 +0000

    Updated language chapter to use better examples and clearer explaination.

---
+++ src/doge_code.c
@@ -0,0 +1,29 @@
+#include "mpc.h"
+
+int main(int argc, char** argv) {
+
+  /* Build a new parser 'Adjective' to recognize descriptions */
+  mpc_parser_t* Adjective = mpc_or(4, 
+    mpc_sym("wow"), mpc_sym("many"),
+    mpc_sym("so"),  mpc_sym("such")
+  );
+
+  /* Build a new parser 'Noun' to recognize things */
+  mpc_parser_t* Noun = mpc_or(5,
+    mpc_sym("lisp"), mpc_sym("language"),
+    mpc_sym("c"),    mpc_sym("book"),
+    mpc_sym("build")
+  );
+  
+  mpc_parser_t* Phrase = mpc_and(2, mpcf_strfold, Adjective, Noun, free);
+  
+  mpc_parser_t* Doge = mpc_many(mpcf_strfold, Phrase);
+
+  /* Do some parsing here... */
+  
+  mpc_delete(Doge);
+  
+  return 0;
+  
+}
+
---
+++ src/doge_grammar.c
@@ -0,0 +1,25 @@
+#include "mpc.h"
+
+int main(int argc, char** argv) {
+
+  mpc_parser_t* Adjective = mpc_new("adjective");
+  mpc_parser_t* Noun      = mpc_new("noun");
+  mpc_parser_t* Phrase    = mpc_new("phrase");
+  mpc_parser_t* Doge      = mpc_new("doge");
+
+  mpc_lang(
+    "                                                                     \
+      adjective : \"wow\" | \"many\" | \"so\" | \"such\";                 \
+      noun      : \"lisp\" | \"language\" | \"c\" | \"book\" | \"build\"; \
+      phrase    : <adjective> <noun>;                                     \
+      doge      : <phrase>*;                                              \
+    ",
+    Adjective, Noun, Phrase, Doge);
+
+  /* Do some parsing here... */
+
+  mpc_cleanup(4, Adjective, Noun, Phrase, Doge);
+  
+  return 0;
+  
+}
