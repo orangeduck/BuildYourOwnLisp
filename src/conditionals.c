5741cb8680322b2005c92a8b1acd95f40b91901d
Daniel Holden <theonlydancingbanana@hotmail.com>
2016-10-18 08:59:38 +0100

    Update conditionals.c

--- src/conditionals.c
+++ src/conditionals.c
@@ -638,10 +638,10 @@ void lenv_add_builtins(lenv* e) {
   lenv_add_builtin(e, "if",    builtin_if);
   lenv_add_builtin(e, "==",    builtin_eq);
   lenv_add_builtin(e, "!=",    builtin_ne);
-  lenv_add_builtin(e, "&gt;",  builtin_gt);
-  lenv_add_builtin(e, "&lt;",  builtin_lt);
-  lenv_add_builtin(e, "&gt;=", builtin_ge);
-  lenv_add_builtin(e, "&lt;=", builtin_le);
+  lenv_add_builtin(e, ">",  builtin_gt);
+  lenv_add_builtin(e, "<",  builtin_lt);
+  lenv_add_builtin(e, ">=", builtin_ge);
+  lenv_add_builtin(e, "<=", builtin_le);
 }
 
 /* Evaluation */
